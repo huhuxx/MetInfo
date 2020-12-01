3827c8c0563b25c31cc6fd884d9b493a9306246f
huhuxx <1647524030@qq.com>
2020-12-01 20:30:54 +0800

    2

--- MetInfo/src/com/hxy/testCase/SearchBox_Test.java
+++ MetInfo/src/com/hxy/testCase/SearchBox_Test.java
@@ -16,40 +16,40 @@ public class SearchBox_Test extends BaseTest {
 	public void SearchBox_set_content_all() throws InterruptedException {	
 		u(driver).set_content_all();	
 	}
-//	@Test
-//	public void SearchBox_set_content_number() throws InterruptedException {	
-//		u(driver).set_content_number();		
-//	}
-//	@Test
-//	public void SearchBox_set_content_symbol() throws InterruptedException {	
-//		u(driver).set_content_symbol();		
-//	}
-//	@Test
-//	public void SearchBox_set_content_text() throws InterruptedException {	
-//		u(driver).set_content_text();		
-//	}
-//	@Test
-//	public void SearchBox_set_range_1() throws InterruptedException {	
-//		u(driver).set_range_1();
-//	}
-//	@Test
-//	public void SearchBox_set_range_2() throws InterruptedException {	
-//		u(driver).set_range_2();		
-//	}
-//	@Test
-//	public void SearchBox_set_style_all() throws InterruptedException {	
-//		u(driver).set_style_all();	
-//	}
-//	@Test
-//	public void SearchBox_set_style_content() throws InterruptedException {	
-//		u(driver).set_style_content();	
-//	}
-//	@Test
-//	public void SearchBox_set_style_tc() throws InterruptedException {	
-//		u(driver).set_style_tc();	
-//	}
-//	@Test
-//	public void SearchBox_set_style_title() throws InterruptedException {	
-//		u(driver).set_style_title();		
-//	}
+	@Test
+	public void SearchBox_set_content_number() throws InterruptedException {	
+		u(driver).set_content_number();		
+	}
+	@Test
+	public void SearchBox_set_content_symbol() throws InterruptedException {	
+		u(driver).set_content_symbol();		
+	}
+	@Test
+	public void SearchBox_set_content_text() throws InterruptedException {	
+		u(driver).set_content_text();		
+	}
+	@Test
+	public void SearchBox_set_range_1() throws InterruptedException {	
+		u(driver).set_range_1();
+	}
+	@Test
+	public void SearchBox_set_range_2() throws InterruptedException {	
+		u(driver).set_range_2();		
+	}
+	@Test
+	public void SearchBox_set_style_all() throws InterruptedException {	
+		u(driver).set_style_all();	
+	}
+	@Test
+	public void SearchBox_set_style_content() throws InterruptedException {	
+		u(driver).set_style_content();	
+	}
+	@Test
+	public void SearchBox_set_style_tc() throws InterruptedException {	
+		u(driver).set_style_tc();	
+	}
+	@Test
+	public void SearchBox_set_style_title() throws InterruptedException {	
+		u(driver).set_style_title();		
+	}
 }
