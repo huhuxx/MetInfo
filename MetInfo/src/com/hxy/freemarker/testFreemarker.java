477eab1d8d70bde3678d66646da6bb2cea7321c6
huhuxx <1647524030@qq.com>
2020-12-15 21:26:40 +0800

    123

--- MetInfo/src/com/hxy/freemarker/testFreemarker.java
+++ MetInfo/src/com/hxy/freemarker/testFreemarker.java
@@ -14,7 +14,7 @@ import freemarker.template.Template;
 import freemarker.template.TemplateException;
 
 public class testFreemarker {
-	private String dir="B:\\Study\\mianxiang\\mian2\\MetInfo";
+	private String dir="C:\Windows\System32\config\systemprofile\AppData\Local\Jenkins\.jenkins\workspace\MetInfo Test2\MetInfo";
 	private Map<String, List<Map<String, Object>>> root = new HashMap<String, List<Map<String, Object>>>();
 	private List<Map<String,Object>> maps = new ArrayList<Map<String,Object>>();
 	private List<Map<String,Object>> maps1 = new ArrayList<Map<String,Object>>();
@@ -70,7 +70,7 @@ public class testFreemarker {
 		
 		Writer out = new FileWriter(dir+"\\freemarkerHtml\\freemarker.html");
 		template.process(root, out);
-		System.out.println("html鍒涘缓鎴愬姛--------------------------------------------");
+		System.out.println("html创建成功--------------------------------------------");
 		out.flush();
 		out.close();
 	}
--- MetInfo/src/com/hxy/testCase/ColumnManage_Test.java
+++ MetInfo/src/com/hxy/testCase/ColumnManage_Test.java
@@ -8,31 +8,31 @@ import com.hxy.Listener.JavaMailTestListener;
 import com.hxy.pageObjects.ColumnManage;
 import com.hxy.util.BaseTest;
 /*
- *  * ºúÑ§Ñî
+ *  * ?????
  */
 @Listeners(JavaMailTestListener.class)
 public class ColumnManage_Test extends BaseTest {
-//	//6¸ö
+//	//6ge1
 	public ColumnManage u(WebDriver webDriver) {
 		ColumnManage a = new ColumnManage(webDriver);
 		return a;
 	}
 	@Test
-	public void ColumnManage_updateTitle() throws InterruptedException {	
-		u(driver).updateTitle();
-	}
-	@Test
-	public void ColumnManage_setDisplay_no() throws InterruptedException {	
-		u(driver).setDisplay_no();
-	}
+//	public void ColumnManage_updateTitle() throws InterruptedException {	
+//		u(driver).updateTitle();
+//	}
+//	@Test
+//	public void ColumnManage_setDisplay_no() throws InterruptedException {	
+//		u(driver).setDisplay_no();
+//	}
 	@Test
 	public void ColumnManage_setDisplay_all() throws InterruptedException {	
 		u(driver).setDisplay_all();
 	}
-	@Test
-	public void ColumnManage_setDisplay_tail() throws InterruptedException {	
-		u(driver).setDisplay_tail();
-	}
+//	@Test
+//	public void ColumnManage_setDisplay_tail() throws InterruptedException {	
+//		u(driver).setDisplay_tail();
+//	}
 	@Test
 	public void ColumnManage_setDisplay_head() throws InterruptedException {	
 		u(driver).setDisplay_head();
